7b27ed3bfc1d32e28d602b096b94cb9f83e4b6b4
wuchang <wuchangfeng@bitbucket.org>
2017-07-11 13:08:55 -0700

    new stuff

--- UrbanRace/u6_all/u6step4.py
+++ UrbanRace/u6_all/u6step4.py
@@ -18,14 +18,15 @@ longdeg=c.u6longdeg
 longref=c.u6longref
 
 s=s.replace(' ','')
-f="73 63 79 74 61 6c 65 20 74 68 69 73\n\n"
-f+=s[0::5]+s[1::5]+s[2::5]+s[3::5]+s[4::5]
+msg1="73 63 79 74 61 6c 65 20 74 68 69 73"
+msg2=s[0::5]+s[1::5]+s[2::5]+s[3::5]+s[4::5]
 img = Image.open("static/concrete_orig.jpg")
 draw = ImageDraw.Draw(img)
 # font = ImageFont.truetype(<font-file>, <font-size>)
 font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono-Bold.ttf",60)
 # draw.text((x, y),"Sample Text",(r,g,b))
-draw.text((20, 20),f,(0,0,0),font=font)
+draw.text((30, 30),msg1,font=font, fill=(0,0,0))
+draw.text((30, 180),msg2,font=font, fill=(0,0,0))
 img.save('static/concrete.jpg')
 command='exiftool -ImageDescription="'+s+'" static/concrete.jpg'
 os.system(command)
